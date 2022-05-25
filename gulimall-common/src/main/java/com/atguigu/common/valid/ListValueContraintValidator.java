5e721b7887714d14b0aef0ec8728b33ae1aaec42
ZM31 <1744930859@qq.com>
2022-05-25 14:26:59 +0800

    品牌管理表单校验OK

---
+++ gulimall-common/src/main/java/com/atguigu/common/valid/ListValueContraintValidator.java
@@ -0,0 +1,26 @@
+package com.atguigu.common.valid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListValueContraintValidator implements ConstraintValidator<ListValue,Integer> {
+    private Set<Integer> set=new HashSet<>();
+    @Override
+    public void initialize(ListValue constraintAnnotation) {
+        int[] value = constraintAnnotation.val();
+        for (int i : value) {
+            set.add(i);
+        }
+
+    }
+
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+
+
+        return  set.contains(value);
+    }
+}
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -1,10 +1,16 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,7 @@ import com.atguigu.gulimall.product.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -58,20 +65,45 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Validated(AddGroup.class) @RequestBody BrandEntity brand/*, BindingResult result*/){
+//        if( result.hasErrors()){
+//            Map<String,String> map=new HashMap<>();
+//            //1.获取错误的校验结果
+//            result.getFieldErrors().forEach((item)->{
+//                //获取发生错误时的message
+//                String message = item.getDefaultMessage();
+//                //获取发生错误的字段
+//                String field = item.getField();
+//                map.put(field,message);
+//            });
+//            return R.error(400,"提交的数据不合法").put("data",map);
+//        }else {
+//
+//        }
+        brandService.save(brand);
 
         return R.ok();
     }
+
 
     /**
      * 修改
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
+    public R update(@Validated(UpdateGroup.class) @RequestBody BrandEntity brand){
 		brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改zhuangtai
+     */
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated(UpdateStatusGroup.class) @RequestBody BrandEntity brand){
+        brandService.updateById(brand);
 
         return R.ok();
     }
